2bfe4a38a0daf09df21ba1113ff2c084608b3742
John Crispin <john@phrozen.org>
2024-03-17 19:34:46 +0100

    foo

--- client.sh
+++ client.sh
@@ -30,6 +30,8 @@ openssl req -new -sha256 -key $1/key.pem -subj "$SUBJ" -out $1/csr.pem
 echo +++++ generate client certificate
 openssl x509 -req -in $1/csr.pem -CA server/ca.pem -CAkey server/ca.key -CAcreateserial -out $1/cert.pem -days $EXPIRE -sha256
 
+openssl pkcs12 -export -out $1/$1.pfx -inkey $1/key.pem -in $1/cert.pem -certfile server/ca.pem
+
 cp server/ca.pem $1
 cd $1
 tar cf ../$1.tar ca.pem cert.pem key.pem
